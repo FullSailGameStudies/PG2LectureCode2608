e06db9c3cde66812a1fcbb1e223b90dd9d43c3b6
Garrett Girod <girodg@hotmail.com>
2026-08-12 10:00:50 -0400

    linear search

--- Week2/Lectures/Searcher.cpp
+++ Week2/Lectures/Searcher.cpp
@@ -3,3 +3,15 @@
 //
 // Part B-1
 //
+
+int Searcher::LinearSearch(const std::vector<Light>& lights, int greenColor) const
+{
+	for (int i = 0; i < lights.size(); i++)
+	{
+		if (greenColor == lights[i].green)
+		{
+			return i;
+		}
+	}
+	return NOT_FOUND;
+}
